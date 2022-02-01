d97884d8c908c252bd5f4a3242b5c6f8e8507bf5
Ingrid Bena Ria <90028955+ingridbena@users.noreply.github.com>
2022-02-02 00:12:48 +0700

    Case Study #1 - Danny's Diner

---
+++ Dannys_Diner.sql
@@ -0,0 +1,22 @@
+SELECT * FROM dannys_diner.sales;
+CREATE TABLE menu (
+  product_id INTEGER,
+  product_name VARCHAR(5),
+  price INTEGER);
+  INSERT INTO menu
+  (product_id, product_name, price)
+VALUES
+  ('1', 'sushi', '10'),
+  ('2', 'curry', '15'),
+  ('3', 'ramen', '12');
+  CREATE TABLE members (
+  customer_id VARCHAR(1),
+  join_date DATE
+);
+
+INSERT INTO members
+  (customer_id, join_date)
+VALUES
+  ('A', '2021-01-07'),
+  ('B', '2021-01-09');
+  
